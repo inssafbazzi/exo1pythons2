cb735b2485de075a77903c858c301dd7cfe78418
inssafbazzi <125572283+inssafbazzi@users.noreply.github.com>
2023-02-16 15:43:06 +0100

    Add files via upload

---
+++ exo1python s2.py
@@ -0,0 +1,23 @@
+#creation de 20 variable different 
+variableInt = 5 # variable entière
+variableFloat = 5.6 # variable décimale
+variableFloat = 5.6e-5 # signifie 5.6x10^-5
+variableString = "chaine" # variable chaine de caractère
+variableBoolean = True
+
+x = 4;
+y = 4;
+
+nbr = 1.44768798798;
+rst = round(nbr); # donne « rst = 1 ».
+nbr = 1.74768798798;
+rst = round(nbr); # donne « rst = 2 ».
+
+nombre = eval(chaine) # Convertie l'entrée de type string en variable numérique
+nombre = int(chaine) # Conversion en int
+nombre = float(chaine) # Conversion en float
+b = bool(var) # Conversion en booléen.
+  
+
+  #collection de variable 
+  a, b, c, d = 3, 23, 'hey!', True # Signifie: a = 3 et b = 23 et c = 'hey !' et d = True  
